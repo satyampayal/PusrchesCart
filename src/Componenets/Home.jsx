d57df8f979edc2f7198b93c3ee15b5dfc0dbeed3
satyam <satyampayal10@gmail.com>
2023-09-14 22:48:00 +0530

    add api

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -15,13 +15,13 @@ function Home() {
         // console.log(cartData);
         setCart(() => cartData);
         console.log(cart)
-        // const cartDiv = cartData.map((item) => {
-        //     return (<div>
-        //         <img src={item.image} alt="image of products" />
-        //         <h1>{item.category}</h1>
-        //         <h1>Rate {item.rating.rate}</h1>
-        //     </div>)
-        // })
+        const cartDiv = cartData.map((item) => {
+            return (<div>
+                <img src={item.image} alt="image of products" />
+                <h1>{item.category}</h1>
+                <h1>Rate {item.rating.rate}</h1>
+            </div>)
+        })
 
         //console.log(cart)
     }
@@ -31,18 +31,18 @@ function Home() {
 
     const cartDiv = cart.map((item) => {
         // const add=false;
-        // return (
-        // <div 
-        // key={item.id}
-        // className='border-[1px] border-gray-300 rounded-[5px] flex flex-col justify-center  gap-[5px] ' >
-        //     <img className='w-[50%] h-[40%] mx-auto' src={item.image} alt="image of products" />
-        //     <h1 className='text-[24px] italic  mx-auto'>{item.category}</h1>
-        //     <h1 className='bg-green-600 text-white rounded-[5px] text-center w-[80%] mx-auto'>Rate {item.rating.rate}</h1>
-        //     <h1 onClick={() =>setAdd(!add)}
-        //         className='bg-blue-600 text-white rounded-[5px] text-center w-[80%]  mx-auto cursor-pointer hover:bg-blue-400'>
-        //         {add ? 'Remove from cart' : 'Add into cart'}
-        //     </h1>
-        // </div>)
+         return (
+        <div 
+        key={item.id}
+        className='border-[1px] border-gray-300 rounded-[5px] flex flex-col justify-center  gap-[5px] ' >
+            <img className='w-[50%] h-[40%] mx-auto' src={item.image} alt="image of products" />
+            <h1 className='text-[24px] italic  mx-auto'>{item.category}</h1>
+            <h1 className='bg-green-600 text-white rounded-[5px] text-center w-[80%] mx-auto'>Rate {item.rating.rate}</h1>
+            <h1 onClick={() =>setAdd(!add)}
+                className='bg-blue-600 text-white rounded-[5px] text-center w-[80%]  mx-auto cursor-pointer hover:bg-blue-400'>
+                {add ? 'Remove from cart' : 'Add into cart'}
+            </h1>
+        </div>)
 
         // <AllCart
         //     key={item.id}
@@ -51,13 +51,14 @@ function Home() {
     })
     return (
         <div className=' px-[30px] mt-[20px] grid grid-cols-5 gap-[20px]'>{
-            cart.map((item) => {
-                <AllCart
-                    key={item.id}
-                    image={item.image}
-                    rate={item.rating.rate}
-                />
-            })
+            // cart.map((item) => {
+            //     <AllCart
+            //         key={item.id}
+            //         image={item.image}
+            //         rate={item.rating.rate}
+            //     />
+            // })
+            cartDiv
         }
         </div>
     )
